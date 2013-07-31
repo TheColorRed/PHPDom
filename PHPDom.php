081a86c277c666912857a69f473015681c9bb1ff
Ryan <Ryan@Ryan-PC>
2013-07-30 22:35:02 -0500

    Added Basic Stylesheet tokenizer

--- PHPDom.php
+++ PHPDom.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . "/classes/URL.php";
+require_once __DIR__ . "/classes/Stylesheet.php";
 
 class PHPDom{
 
@@ -10,6 +11,12 @@ class PHPDom{
     protected $httpCode    = "";
     protected $stylesheets = array("links"  => array(), "markup" => array());
 
+    /**
+     * Loads a web pages html and css
+     * 
+     * @param String $url The website to get the data from
+     * @param Array $other_params An array of extra curl parameters
+     */
     public function loadURL($url, $other_params = array()){
         $this->url  = $url;
         $u          = new URL($url);
@@ -27,9 +34,12 @@ class PHPDom{
         foreach($matches[1] as $url){
             $this->stylesheets["links"][] = URL::rel2abs($url, $this->url);
         }
-        $url = new URL($this->stylesheets["links"]);
-        $this->stylesheets["markup"] = $url->go();
-        
+        $url         = new URL($this->stylesheets["links"]);
+        $stylesheets = $url->go();
+        foreach($stylesheets as $markup){
+            $stylesheet                    = new Stylesheet();
+            $this->stylesheets["markup"][] = $stylesheet->loadStylesheet($markup);
+        }
     }
 
 }
---
+++ classes/Stylesheet.php
@@ -0,0 +1,64 @@
+<?php
+
+class Stylesheet{
+
+    protected $stylesheet = "";
+    protected $markup     = array();
+
+    public function loadStylesheet($string){
+        $this->stylesheet = $string;
+        $this->parse();
+        return $this->markup;
+    }
+
+    public function parse(){
+        $this->cleanup();
+        $length     = strlen($this->stylesheet);
+        $tokens     = array();
+        $selector   = "";
+        $property = "";
+        $isSelector = true;
+        $string = "";
+        for($i = 0; $i < $length; $i++){
+            $ch = $this->stylesheet[$i];
+            switch($ch){
+                case "{":
+                    $selector = $string;
+                    $string = "";
+                    $tokens[$selector] = array();
+                    $isSelector = false;
+                    break;
+                case ":":
+                    $property = trim($string);
+                    $string = "";
+                    $tokens[$selector][$property] = "";
+                    break;
+                case ";":
+                    $tokens[$selector][$property] = trim($string);
+                    $string = "";
+                    break;
+                case "}":
+                    $selector = "";
+                    $string = "";
+                    break;
+                default:
+                    $string .= $ch;
+                    break;
+            }
+        }
+        $this->markup = $tokens;
+        unset($tokens);
+    }
+
+    protected function cleanup(){
+        $sheet            = $this->stylesheet;
+        // Remove Comments
+        $sheet            = preg_replace("/\/\*.+\*\//isU", "", $sheet);
+        // Remove Tabs, New Lines, and Carriage Returns
+        $sheet            = preg_replace("/\t|\r|\n/", "", $sheet);
+        //echo $sheet."\n\n\n\n\n\n\n";
+        $this->stylesheet = $sheet;
+        unset($sheet);
+    }
+
+}
